48d6f7864dcb235ec35edb310339541136396211
DerryZZ <zdr51888@163.com>
2021-06-17 14:37:50 +0800

    猜数字   1->100

---
+++ caishuzi_6_17/caishuzi_6_17/caishuzi.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<Windows.h>
+void menu()
+{
+	printf("###############################\n");
+	printf("###########猜数字游戏##########\n");
+	printf("###############################\n");
+	printf("######  1.开始   0.退出  ######\n");
+	printf("###############################\n");
+}
+void game()
+{
+	//生成随机数字
+	int shu = 0;
+	int cai = 0;
+	shu = rand();//生成随机数
+	shu = shu % 100 + 1;
+	printf("请猜一个数:>");
+	scanf("%d", &shu);
+	//猜数字
+	
+	while (1)
+	{
+
+		if (cai == shu)
+		{
+			system("cls");
+			printf("猜对了\n");
+			Sleep(3000);
+			system("cls");
+			break;
+		}
+		else if (cai > shu)
+		{
+			printf("猜大了\n");
+			scanf("%d", &cai);
+		}
+		else
+		{
+			printf("猜小了\n");
+			scanf("%d", &cai);
+		}
+	}
+}
+int main()
+{
+	int a=0;
+	srand((unsigned int)time(NULL));
+	
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &a);
+		switch (a)
+		{
+		case 1:
+			game();
+			break;
+		default:
+			printf("请输入正确选项：");
+			scanf("%d", &a);
+			break;
+		case 0:
+			break;
+		}
+	} while (a);
+	return 0;
+}
